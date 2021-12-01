8e369bf4c2e9a12664924d13583e76eb81588c36
felipendc <felipendc10@gmail.com>
2021-11-30 21:19:42 -0300

    Vicyos Rolling Update: (Tue Nov 30 09:19:40 PM -03 2021)

--- generating-ssh-keys-for-github.sh
+++ generating-ssh-keys-for-github.sh
@@ -4,7 +4,7 @@ ssh-keygen -t ed25519 -C "felipendc10@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
-echo "Copie essa chave vá até: GITHUB - settings/SSH and GPG keys"
-echo "Clica em 'Nova chave SSH'"
+echo "Copy this key and go to: GITHUB - settings/SSH and GPG keys"
+echo "Click the 'New SSH key' button"
 cat ~/.ssh/id_ed25519.pub
 
