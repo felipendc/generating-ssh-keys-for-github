1c7e8230fe4bf88842e2888e11cc13b5bc35c7ee
felipendc <felipendc10@gmail.com>
2021-11-30 22:34:16 -0300

    Vicyos Rolling Update: (Tue Nov 30 10:34:13 PM -03 2021)

--- testing-my-github-ssh-key.sh
+++ testing-my-github-ssh-key.sh
@@ -2,4 +2,8 @@
 # Script automated by Felipe Ndc aka Vicyos
 
 # Testing my SSH connection:
+echo " "
+echo "================================================================================"
 ssh -T git@github.com
+echo "================================================================================"
+echp " "
